f1fe20c930ae0432dc4f732f9655db798a131f91
khactu214 <44285970+khactu214@users.noreply.github.com>
2020-06-15 17:41:20 +0700

    asd

--- springboot-05(Them Search)/src/main/java/com/example/springboot05/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.springboot05.repository;
-
-import com.example.springboot05.enity.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-    Iterable<User> findByname(String name);
-}
